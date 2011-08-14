ebbb7124fa19163ec28005ba34dbee016d488b08
Kimie Furuya <kimie.f0714@gmail.com>
2011-08-14 23:42:24 +0900

    I made a sorting programm.

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,44 @@
 # implement this
+package Sorter;
+
+sub new{
+   my $class = shift;
+   my $self = {};
+   @array = ();
+   return bless $self,$class;
+}
+
+sub set_values{
+	my $class;
+	($class,@array) = @_;
+}
+
+sub get_values{
+	@array;
+}
+
+sub sort{
+	&Qsort(0,@array-1,@array);
+}
+
+sub Qsort{
+	my $left; my $right;
+	($left,$right,@array) = @_;
+	
+	my $i=$left;
+	my $j=$right;
+	my $pivot = $array[int(($left + $right) /2)];
+	while(1){
+		while ($array[$i] < $pivot){ $i++;}
+		while ($pivot < $array[$j]){ $j--;}
+		if($j <= $i){ last;}
+		
+		($array[$i],$array[$j])=($array[$j],$array[$i]);
+		
+		$i++; $j--;
+	}
+	if($left < $i-1) {&Qsort($left, $i-1, @array);}
+	if($j+1 < $right) {&Qsort($j+1, $right, @array);}
+}
+
+1; # DO NOT FORGET THIS!!
