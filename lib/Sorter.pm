efa484d14414c3eee3f887f65979b74f140ae6cc
Kimie Furuya <kimie.f0714@gmail.com>
2011-08-19 06:23:27 +0900

    test passed.

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -13,37 +13,88 @@ sub new{
 }
 
 sub set_values{
-	my $self = shift;
-	( @{ $self->{array} } )= @_;
+    my $self = shift;
+	@{ $self->{array} } = @_;
 }
 
 sub get_values{
-    my ( $self ) = @_; 
+    my $self = shift;
     @{ $self->{array} };
 }
 
 sub sort{
-    my ( $self ) = @_;
+    my $self = shift;
     if ( @{ $self->{array} } == 0 ) { return; }
-    warn "要素数-1: " . @{ $self->{array} }-1;
-    $self->Qsort( 0, @{ $self->{array} }-1 );
+    &quick_sort($self->{array}, 0, @{ $self->{array} } - 1);
+}
+
+sub quick_sort {
+    my ($list, $lo, $hi) = @_;
+    if ( $hi <= $lo ) {
+        return;
+    }
+
+    # Pick the value at the last of the list as a pivot
+    my $pivot_value = $list->[$hi];
+
+    my $smaller_pos = $lo;
+    my $larger_pos = $hi - 1; # last one taken as a pivot
+
+    while ($smaller_pos < $larger_pos) {
+
+        # check if the value should be swapped
+        my $swap_left = $list->[$smaller_pos] >= $pivot_value? 1 : 0;
+        my $swap_right = $list->[$larger_pos] < $pivot_value? 1 : 0;
+
+        # if we found two values to be swapped, swap them.
+        if ($swap_left && $swap_right) {
+            &swap($list, $smaller_pos, $larger_pos);
+            $swap_left = $swap_right = 0;
+        }
+
+        # if the value needs not be swapped, we move on.
+        $smaller_pos++ unless $swap_left;
+        $larger_pos-- unless $swap_right;
+    }
+
+    # pivot needs to be inserted at the appropriate pos.
+    if ( $list->[$smaller_pos] < $pivot_value ) {
+        $smaller_pos++;
+    }
+    &swap($list, $hi, $smaller_pos);
+
+    # recursively apply quick_sort
+    quick_sort($list, $lo, $smaller_pos - 1);
+    quick_sort($list, $smaller_pos + 1, $hi);
+}
+
+# スワップ関数
+# - $list[i] と $list[j] を入れ替える
+# input: $i, $j
+# pre: $i, $j ∈ integer and 0 ≦ $i, $j < @lst - 1,
+# post: $list[i] = $list[j] and $list[j] = $list[i]
+sub swap{
+    my ($list, $i, $j) = @_;
+    my $temp = $list->[$i];
+    $list->[$i] = $list->[$j];
+    $list->[$j] = $temp;
 }
 
 sub Qsort{
 	my ( $self, $left, $right ) = @_;
-	my ( @array ) = ( @{ $self->{array} } );
-
-	my $i = $left;
-	warn "i: " . $i;
 	$right = 0 if $right < 0;
-	my $j = $right;
-	warn $j;
+	my ( @array ) =( @{ $self->{array} } );
+
+	my ( $i, $j ) = ( $left, $right );
+#	warn "i: " . $i;
+#	warn "j: " . $j;
 	my $pivot = $array[ int( ( $left + $right ) /2 ) ];
-	warn $array[ int( ( $left + $right ) /2 ) ];
+#	warn "pivot: " . $pivot;
 	while(1){
 		while ( $array[ $i ] < $pivot ){ $i++; }
 		while ( $pivot < $array[ $j ] ){ $j--; }
-		if ( $j <= $i ) { last; }
+		
+		if( $j <= $i ) { last;}
 		
 		( $array[ $i ], $array[ $j ] ) = ( $array[ $j ], $array[ $i ] );
 		
--- t/sort.t
+++ t/sort.t
@@ -1,7 +1,5 @@
 package test::Sorter;
-BEGIN {
-push (@INC, '../lib');
-}
+
 use strict;
 use warnings;
 use base qw(Test::Class);
@@ -14,7 +12,7 @@ sub init : Test(1) {
 
 sub values : Tests {
     my $sorter = Sorter->new;
-    is_deeply [$sorter->get_values], [];
+    is_deeply [$sorter->get_values], [], 'ソートなしリストがからのとき';
 
     $sorter->set_values;
     is_deeply [$sorter->get_values], [];
@@ -37,27 +35,27 @@ sub sort : Tests {
 
     $sorter->set_values(5,4,3,2,1);
     $sorter->sort;
-    is_deeply [$sorter->get_values], [1,2,3,4,5], 'リスト要素５こ';
+    is_deeply [$sorter->get_values], [1,2,3,4,5], 'ソート後リスト要素５こ';
 
-    # $sorter->set_values(-1,-2,-3,-4,-5);
-    # $sorter->sort;
-    # is_deeply [$sorter->get_values], [-5,-4,-3,-2,-1];
+     $sorter->set_values(-1,-2,-3,-4,-5);
+     $sorter->sort;
+     is_deeply [$sorter->get_values], [-5,-4,-3,-2,-1];
 
-    # $sorter->set_values(1,2,3,4,5);
-    # $sorter->sort;
-    # is_deeply [$sorter->get_values], [1,2,3,4,5];
+     $sorter->set_values(1,2,3,4,5);
+     $sorter->sort;
+     is_deeply [$sorter->get_values], [1,2,3,4,5];
 
-    # $sorter->set_values(5,5,4,4,4,3,2,1);
-    # $sorter->sort;
-    # is_deeply [$sorter->get_values], [1,2,3,4,4,4,5,5]; 
+     $sorter->set_values(5,5,4,4,4,3,2,1);
+     $sorter->sort;
+     is_deeply [$sorter->get_values], [1,2,3,4,4,4,5,5]; 
 
-    # for (0..4) {
-    #     my @random_values = ();
-    #     push(@random_values, int(rand() * 100) - 50)  for 0..99;
-    #     $sorter->set_values(@random_values);
-    #     $sorter->sort;
-    #     is_deeply [$sorter->get_values], [sort { $a <=> $b } @random_values];
-    # }
+     for (0..4) {
+         my @random_values = ();
+         push(@random_values, int(rand() * 100) - 50)  for 0..99;
+         $sorter->set_values(@random_values);
+         $sorter->sort;
+         is_deeply [$sorter->get_values], [sort { $a <=> $b } @random_values];
+     }
 }
 
 __PACKAGE__->runtests;
